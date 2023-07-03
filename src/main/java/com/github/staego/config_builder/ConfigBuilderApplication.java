cf36ea595063e3ba03a8eca4af96192939c9ceee
staego <staego@yandex.ru>
2023-07-03 12:17:28 +0300

    Initial commit

---
+++ src/main/java/com/github/staego/config_builder/ConfigBuilderApplication.java
@@ -0,0 +1,13 @@
+package com.github.staego.config_builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConfigBuilderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConfigBuilderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/staego/config_builder/ConfigBuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.staego.config_builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigBuilderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
